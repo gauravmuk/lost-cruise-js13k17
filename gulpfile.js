b8285243529eee5a70f8d14cd43a919182961b9b
Gaurav Nanda <gaurav.nanda@wingify.com>
2017-09-10 22:06:02 +0530

    Add build

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('build', function () {
+    return gulp.src([
+        'src/events/Keyboard.js',
+        'src/utils/Utils.js',
+        'src/model/Camera.js',
+        'src/model/Bullet.js',
+        'src/model/Wall.js',
+        'src/model/Ship.js',
+        'src/model/Background.js',
+        'src/factory/GameFactory.js',
+        'src/paint/CollisionManager.js',
+        'src/paint/WallsController.js',
+        'src/paint/AnimationFrameController.js',
+        'src/GameMaker.js'
+    ])
+        .pipe(concat('ship.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
+});
